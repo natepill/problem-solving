2b4c0451337150c84277d2d24b0d6c654202b94e
Nathan Pillai <natepill@gmail.com>
2019-05-24 13:20:47 -0400

    [added] variable counting number of programming running

--- PracticePython/cocurrent_programs.py
+++ PracticePython/cocurrent_programs.py
@@ -8,13 +8,15 @@ def max_programs_being_run(timeframes):
     """Return the maximum amount of programs that were running at one time."""
 
     current_time_frame = None
+    num_of_programs_running = 0
+
 
     for timeframe in timeframes:
 
         if timeframe[0] >= current_time_frame:
             # end of current
 
-    
+
 
 input = [(1,3), (1,4), (2,6), (4,7), (5,8)]
 # Answer: 3
