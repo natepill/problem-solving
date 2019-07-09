b7086fd8b9d2777a882e8a87f435811093c39b39
Nathan Pillai <natepill@gmail.com>
2019-07-08 17:46:50 -0700

    [added] integer to roman

--- LeetCode/longest_common_prefix.py
+++ LeetCode/longest_common_prefix.py
@@ -1,27 +1,49 @@
-def longestCommonPrefix(strs: List[str]) -> str:
-"""
-Write a function to find the longest common prefix string amongst an array of strings.
-If there is no common prefix, return an empty string "".
-"""
-    prefix = list()
+def longestCommonPrefix(strs):
+    """
+    Write a function to find the longest common prefix string amongst an array of strings.
+    If there is no common prefix, return an empty string "".
+    """
+
+    prefix = strs[0]
+
     for word in strs:
-        curr_prefix = list()
 
-        for index in range(0, len(word)-1):
+        curr_prefix = ''
 
-            if word[index] == prefix[index]:
-                curr_prefix.append(word[index])
-            else:
+        print("word:", word)
+
+        for index in range(0, len(word)):
+
+            # if len(prefix)
+            print("index:", index)
+            print("word[index]:", word[index])
+            # print("prefix[index]:", prefix[index])
+            print("curr_prefix:", curr_prefix)
+            # ["aca","cba"]
+            try:
+                if word[index] == prefix[index]:
+                    curr_prefix += word[index]
+                else:
+                    break
+            except:
                 break
 
+            print("curr_prefix:", curr_prefix)
 
+        if len(curr_prefix) <= len(prefix):
+            prefix = curr_prefix
+
+
+    return prefix
 
 
 if __name__ == "__main__":
 
 
-    # input =
+    # input = ["rfnlower","efwlow","wfqlight","qfwlip"]
+    # input = ["al", 'alp', 'alpha']
+    input = ["aca","cba"]
     print(longestCommonPrefix(input))
 
-# Input: ["flower","flow","flight"]
-# Output: "fl"
+    # input: ["flower","flow","flight"]
+    # Output: "fl"
