0f00d354955a993e350f8f635c0af01041d642f8
Nathan Pillai <natepill@gmail.com>
2019-09-03 19:41:24 -0700

    [added] rewrote exactly what i coded to solve solution

--- Canvas/min_parenthesis.py
+++ Canvas/min_parenthesis.py
@@ -1,19 +1,53 @@
 def minAddToMakeValid(S):
+
   stack = []
   counter = 0
 
+
+
   for char in S:
-    if char == '(':
-    
-      stack.append(char)
-    elif char == ')':
-      try:
-        stack.pop()
-      except:
-        counter += 1
-        continue
+    print("char:", char)
+    # if char == '(':
+    #   print("HELLO")
+    #   stack.append(char)
+    # elif char == ')':
+    #   try:
+    #     stack.pop()
+    #   except:
+    #     counter += 1
+    #     continue
+    # else:
+    #     continue
+    #
+    # print(stack)
+
     return counter+len(stack)
 
 
+
 sampleInput = '((('
-print(minAddToMakeValid(sampleInput))
+# print(minAddToMakeValid(sampleInput))
+
+
+# for char in sampleInput:
+#     print('(')
+
+def test(S):
+    stack = []
+    counter = 0
+    for char in S:
+        if char == '(':
+            stack.append(char)
+            print(stack)
+        elif char == ')':
+            try:
+                stack.pop()
+            except:
+                counter += 1
+                continue
+        else:
+            continue
+
+    return counter + len(stack)
+
+print(test(sampleInput))
