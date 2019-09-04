708c05df50abe12e5e5f467dd289d69deddc9f21
Nathan Pillai <natepill@gmail.com>
2019-09-03 19:42:58 -0700

    [edit] commented out wrong solution

--- Canvas/min_parenthesis.py
+++ Canvas/min_parenthesis.py
@@ -1,23 +1,23 @@
-def minAddToMakeValid(S):
-
-  stack = []
-  counter = 0
-
-  for char in S:
-    print("char:", char)
-    if char == '(':
-      print("HELLO")
-      stack.append(char)
-    elif char == ')':
-      try:
-        stack.pop()
-      except:
-        counter += 1
-        continue
-    else:
-        continue
-
-    print(stack)
+# def minAddToMakeValid(S):
+#
+#   stack = []
+#   counter = 0
+#
+#   for char in S:
+#     print("char:", char)
+#     if char == '(':
+#       print("HELLO")
+#       stack.append(char)
+#     elif char == ')':
+#       try:
+#         stack.pop()
+#       except:
+#         counter += 1
+#         continue
+#     else:
+#         continue
+#
+#     print(stack)
 
     return counter+len(stack)
 
