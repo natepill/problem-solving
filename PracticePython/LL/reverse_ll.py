c8b6baeb208956e333dda1871d5d14cc2b4b4bca
Nathan Pillai <natepill@gmail.com>
2020-02-01 16:45:20 -0800

    [added] reverse LL

--- PracticePython/LL/reverse_ll.py
+++ PracticePython/LL/reverse_ll.py
@@ -5,66 +5,33 @@
     Review the recursive solution
 
 """
-
-
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-def reverseList(head: ListNode) -> ListNode:
-
-    # prev_node (None)
-    # curr_node
-    # next_node
-    prev_node = None
-    curr_node = head
-    next_node = head.next
-
-    while next_node is not None:
-        # point curr_node towards prev_node
-        curr_node.next = prev_node
-        # set prev_node to curr_node
-        prev_node = curr_node
-        # curr_node set to next_node
-        curr_node = next_node
-        # next_node set to curr_node.next
-        next_node = curr_node.next
-
-
-    curr_node.next = prev_node
-
-    return curr_node
-
-
-
-
-
-
-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
-
-    def helper(self, prev, cur):
-
-        if cur:
-
-            # locate next hopping node
-            next_hop = cur.next
-
-            # reverse direction
-            cur.next = prev
-
-            return self.helper( cur, next_hop)
-
-        else:
-
-            # new head of reverse linked list
-            return prev
-
-
     def reverseList(self, head: ListNode) -> ListNode:
 
-        return self.helper( None, head)
+        # init backwards to None
+
+        # keep reversing until end of LL
+            # store next pointer
+            # point curr_node backwards
+            # backwards becomes curr_node
+            # curr_node becomes next_node
+
+        # node pointers
+        prev_node = None
+        curr_node = head
+        # next_node = head.next
+
+        # keep reversing until end of LL
+        while curr_node is not None:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+
+        return prev_node
