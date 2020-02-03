1ebdae321ec84e1089104d7b984ce05ef6dc8e1c
Nathan Pillai <natepill@gmail.com>
2020-02-03 10:35:21 -0800

    [added] rotate arr algo

--- PracticePython/Arrays/rotate_arr.py
+++ PracticePython/Arrays/rotate_arr.py
@@ -1,49 +1,59 @@
-def rotate(nums, k) -> None:
-    """
-    Do not return anything, modify nums in-place instead.
-    """
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
-    og_k = k
-    k -=1
-    # iterate over backwards range of k indicies
-    for i in range(-1, -k-2, -1):
+#         Steps to Classical Rotation
+#         reverse the first n - k elements
 
-        # swap each val at backwards indicie at k indicie
-        nums[k], nums[i] = nums[i], nums[k]
+#         reverse the rest of them
 
-        k -= 1
+#         reverse the entire array
+        if len(nums) == 1:
+            return nums
 
-    for i in range(og_k, len(nums)-og_k):
-        print(i)
-        nums.insert(len(nums), nums.pop(og_k))
+        nums[0:len(nums)-k] = reversed(nums[0:len(nums)-k])
 
+        nums[len(nums)-k:] = reversed(nums[len(nums)-k:])
 
-# def rotate(self, nums: List[int], k: int) -> None:
-#     """
-#     Do not return anything, modify nums in-place instead.
-#     """
-#     k %= len(nums)
-#     nums.reverse()
-#
-#     i = 0
-#     j = k-1
-#     while i < j:
-#         nums[i], nums[j] = nums[j], nums[i]
-#         i += 1
-#         j -= 1
-#
-#     nums[k:] = reversed(nums[k:])
+        nums[:] = reversed(nums)
+
+        return nums
 
 
+#         if len(nums) == 1:
+#             return nums
 
+#         if len(nums) == 2:
+#             # print("HERE")
+#             if k%2 == 1:
+#                 nums[0], nums[1] = nums[1], nums[0]
+#                 return nums
+#             else:
+#                 return nums
 
+#         nums[:] = reversed(nums)
 
+#         nums[0:k] = reversed(nums[0:k])
 
+#         nums[k:len(nums)] = reversed(nums[k:len(nums)])
 
-if __name__ == "__main__":
-    arr = [1,2,3,4,5,6,7,8]
-    rotate(arr, 3)
-    print(arr)
-    #
-    # for i in range(-1, -3-1, -1):
-    #     print(i)
+#         return nums
+
+#         if nums is None or len(nums)==1:
+#             return nums
+#         # iterate over backwards range of k indicies
+#         og_k = k
+#         k -=1
+#         # iterate over backwards range of k indicies
+#         for i in range(-1, -k-2, -1):
+
+#             # swap each val at backwards indicie at k indicie
+#             nums[k], nums[i] = nums[i], nums[k]
+
+#             k -= 1
+
+#         for i in range(og_k, len(nums)-og_k):
+#             print(i)
+#             nums.insert(len(nums), nums.pop(og_k))
